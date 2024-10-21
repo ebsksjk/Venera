8a9eccf54881fd211469ec1ac66298a0433789d4
Mara <mara@mara.mara>
2024-10-21 08:56:30 +0200

    ähhhhh

--- VoPo/ELF.cs
+++ VoPo/ELF.cs
@@ -1,193 +1,138 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
-
-
+using System.Collections.Generic;
+using CosmosELFCore;
 
 namespace Venera.VoPo
 {
-
-    internal class ELF
+    // ELF header structure (64-bit ELF)
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct ELFHeader
     {
-        Elf32_Ehdr header;
-        public unsafe ELF(byte[] data)
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+        public byte[] e_ident;     // Magic number and other info
+        public ushort e_type;      // Object file type
+        public ushort e_machine;   // Architecture
+        public uint e_version;     // Object file version
+        public ulong e_entry;      // Entry point virtual address
+        public ulong e_phoff;      // Program header table file offset
+        public ulong e_shoff;      // Section header table file offset
+        public uint e_flags;       // Processor-specific flags
+        public ushort e_ehsize;    // ELF header size in bytes
+        public ushort e_phentsize; // Program header table entry size
+        public ushort e_phnum;     // Program header table entry count
+        public ushort e_shentsize; // Section header table entry size
+        public ushort e_shnum;     // Section header table entry count
+        public ushort e_shstrndx;  // Section header string table index
+    }
+
+    // Section header structure (64-bit ELF)
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct SectionHeader
+    {
+        public uint sh_name;       // Section name (index into string table)
+        public uint sh_type;       // Section type
+        public ulong sh_flags;     // Section flags
+        public ulong sh_addr;      // Virtual address in memory
+        public ulong sh_offset;    // Offset of the section in the file
+        public ulong sh_size;      // Size of the section in bytes
+        public uint sh_link;       // Section index link
+        public uint sh_info;       // Extra information
+        public ulong sh_addralign; // Alignment of the section
+        public ulong sh_entsize;   // Size of entries if the section holds a table
+    }
+
+    // Program header structure (64-bit ELF)
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct ProgramHeader
+    {
+        public uint p_type;        // Type of segment
+        public uint p_flags;       // Segment attributes
+        public ulong p_offset;     // Offset in file
+        public ulong p_vaddr;      // Virtual address in memory
+        public ulong p_paddr;      // Physical address (for systems using physical memory)
+        public ulong p_filesz;     // Size of segment in file
+        public ulong p_memsz;      // Size of segment in memory
+        public ulong p_align;      // Alignment of segment
+    }
+
+    class ELFFile
+    {
+        public ELFHeader header { get; set; }
+        public List<SectionHeader> sheader { get; set; } = new List<SectionHeader>();
+        public List<ProgramHeader> pheader { get; set; } = new List<ProgramHeader>();
+        private List<uint> _stringTables = new List<uint>();
+
+        public ELFFile(byte[] elffile)
         {
-            fixed (byte* ptr = data)
+            if (elffile.Length == 0)
             {
-                header = *(Elf32_Ehdr*)ptr;
+                return;
+            }
+
+            try
+            {
+                unsafe
+                {
+                    fixed (byte* ptr = elffile)
+                    {
+                        CosmosELFCore.MemoryStream ms = new CosmosELFCore.MemoryStream(ptr);
+                        CosmosELFCore.BinaryReader reader = new CosmosELFCore.BinaryReader(ms);
+                            // Read and parse ELF header
+                            ELFHeader elfHeader = ReadStruct<ELFHeader>(reader);
+
+                            // Check if it is a valid ELF file
+                            if (elfHeader.e_ident[0] != 0x7F || elfHeader.e_ident[1] != (byte)'E' || elfHeader.e_ident[2] != (byte)'L' || elfHeader.e_ident[3] != (byte)'F')
+                            {
+                                Kernel.PrintDebug("Not a valid ELF file.");
+                                return;
+                            }
+
+                            Kernel.PrintDebug("ELF Header:");
+                            Kernel.PrintDebug($"  Type: {elfHeader.e_type}");
+                            Kernel.PrintDebug($"  Machine: {elfHeader.e_machine}");
+                            Kernel.PrintDebug($"  Entry Point: 0x{elfHeader.e_entry:X}");
+
+                            // Read and parse section headers
+                            ms.Seek((long)elfHeader.e_shoff, SeekOrigin.Begin);
+                            for (int i = 0; i < elfHeader.e_shnum; i++)
+                            {
+                                SectionHeader sectionHeader = ReadStruct<SectionHeader>(reader);
+                                Kernel.PrintDebug($"Section {i}:");
+                                Kernel.PrintDebug($"  Offset: 0x{sectionHeader.sh_offset:X}");
+                                Kernel.PrintDebug($"  Size: 0x{sectionHeader.sh_size:X}");
+                                Kernel.PrintDebug($"  Type: {sectionHeader.sh_type}");
+                            }
+
+                            // Read and parse program headers
+                            ms.Seek((long)elfHeader.e_phoff, SeekOrigin.Begin);
+                            for (int i = 0; i < elfHeader.e_phnum; i++)
+                            {
+                                ProgramHeader programHeader = ReadStruct<ProgramHeader>(reader);
+                                Kernel.PrintDebug($"Program Header {i}:");
+                                Kernel.PrintDebug($"  Offset: 0x{programHeader.p_offset:X}");
+                                Kernel.PrintDebug($"  Size in File: 0x{programHeader.p_filesz:X}");
+                                Kernel.PrintDebug($"  Size in Memory: 0x{programHeader.p_memsz:X}");
+                                Kernel.PrintDebug($"  Type: {programHeader.p_type}");
+                            } 
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Kernel.PrintDebug($"Error: {ex.Message}");
             }
         }
 
-        public Elf32_Ehdr getHeader(){
-
-                return header;
-        }
-
-    }
-
-    //Elf 32 bit symbol table entry
-    [StructLayout(LayoutKind.Explicit, Pack = 1)]
-    public struct Elf32_Sym
-    {
-        [FieldOffset(0)] public uint st_name;
-        [FieldOffset(4)] public uint st_value;
-        [FieldOffset(8)] public uint st_size;
-        [FieldOffset(12)] public byte st_info;
-        [FieldOffset(13)] public byte st_other;
-        [FieldOffset(14)] public ushort st_shndx;
-    }
-
-    //relocation table entry
-    [StructLayout(LayoutKind.Explicit, Pack = 1)]
-    public struct Elf32_Rel
-    {
-        [FieldOffset(0)] public uint r_offset;
-        [FieldOffset(4)] public uint r_info;
-    }
-
-    //ELF Header struct
-    [StructLayout(LayoutKind.Explicit, Pack = 1)]
-    public unsafe struct Elf32_Ehdr
-    {
-        [FieldOffset(0)] public fixed byte e_ident[16]; //stuff for identification -> index 4 should be 1, we're 32 bit
-        [FieldOffset(16)] public ushort e_type;         //type of elf file
-        [FieldOffset(18)] public ushort e_machine;      //type. probably 3 ig (Intel 386)
-        [FieldOffset(20)] public uint e_version;        //version (should be 1)
-        [FieldOffset(24)] public uint e_entry;          //entry point address. or zero. zero is bad
-        [FieldOffset(28)] public uint e_phoff;          //program header table offset from beginning of file
-        [FieldOffset(32)] public uint e_shoff;          //section header table offset from beginning of file
-        [FieldOffset(36)] public uint e_flags;          //machine flags or smth
-        [FieldOffset(40)] public ushort e_ehsize;       //size of this header
-        [FieldOffset(42)] public ushort e_phentsize;    //program header table entry size
-        [FieldOffset(44)] public ushort e_phnum;        //program header table entry num
-        [FieldOffset(46)] public ushort e_shentsize;    //section header table entry size
-        [FieldOffset(48)] public ushort e_shnum;        //section header table entry num
-        [FieldOffset(50)] public ushort e_shstrndx;     //section header table index of the section name string table (what?)
-    }
-
-    public enum ELFType
-    {
-        ET_NONE = 0,            //no file type
-        ET_REL = 1,             //relocatable file
-        ET_EXEC = 2,            //executable file <-- we want this :3
-        ET_DYN = 3,             //shared object file
-        ET_CORE = 4,            //core file
-        ET_LOPROC = 0xff00,     //processor spefic
-        ET_HIPROC = 0xffff      // ''
-    }
-
-
-    [StructLayout(LayoutKind.Explicit, Pack = 1)]
-    public struct Elf32_Shdr
-    {
-        [FieldOffset(0)] public uint sh_name;       //name (index of an c string in the string table)
-        [FieldOffset(4)] public uint sh_type;       //Section Type (see SectionType)
-        [FieldOffset(8)] public uint sh_flags;      //weird flags
-        [FieldOffset(12)] public uint sh_addr;      //is this section interesting? then put it at the position specified here. if not, it is 0
-        [FieldOffset(16)] public uint sh_offset;    //offset of the corresponding section
-        [FieldOffset(20)] public uint sh_size;      //size of the corresponding section
-        [FieldOffset(24)] public uint sh_link;      //index of a section header in the section header table. if this section is a symbol table, this is the index of the string table
-        [FieldOffset(28)] public uint sh_info;      //additional info
-        [FieldOffset(32)] public uint sh_addralign; //alignment
-        [FieldOffset(36)] public uint sh_entsize;   //entrysize
-    }
-
-    public enum SectionType
-    {
-        None = 0,
-        ProgramInformation = 1,
-        SymbolTable = 2,
-        StringTable = 3,
-        RelocationAddend = 4,
-        NotPresentInFile = 8,
-        Relocation = 9,
-    }
-
-    [Flags]
-    public enum SectionAttributes
-    {
-        Write = 0x01,
-        Alloc = 0x02,
-        Executable = 0x4
-    }
-
-
-
-    //symbol table entry
-    public unsafe class Elf32Sym
-    {
-        public string Name;
-        public uint NameOffset; //index of the name in the string table
-        public uint Value;      //value of the symbol
-        public uint Size;       //size of the symbol
-        public byte Info;       //info (see also binding and type)
-        public byte Other;      //0. just 0.
-        public ushort Shndx;    //section header index
-        public SymbolBinding Binding;
-        public SymbolType Type;
-
-        public Elf32Sym(Elf32_Sym* st)
+        // Helper method to read a structure from the binary reader
+        private static T ReadStruct<T>(CosmosELFCore.BinaryReader reader)
         {
-            NameOffset = st->st_name;
-            Value = st->st_value;
-            Size = st->st_size;
-            Info = st->st_info;
-            Other = st->st_other;
-            Shndx = st->st_shndx;
-
-            Binding = (SymbolBinding)(Info >> 0x4);
-            Type = (SymbolType)(Info & 0x0F);
+            byte[] bytes = reader.ReadBytes(Marshal.SizeOf<T>());
+            GCHandle handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            T theStruct = Marshal.PtrToStructure<T>(handle.AddrOfPinnedObject());
+            handle.Free();
+            return theStruct;
         }
     }
-
-    public enum SymbolBinding
-    {
-        Local = 0, // Local scope
-        Global = 1, // Global scope
-        Weak = 2  // Weak, (ie. __attribute__((weak)))
-    }
-
-    public enum SymbolType
-    {
-        None = 0, // No type
-        Object = 1, // Variables, arrays, etc.
-        Function = 2,  // Methods or functions
-        Common = 5
-    }
-
-
-    public unsafe class Elf32Shdr
-    {
-        public string Name;
-        public uint NameOffset;
-        public SectionType Type;
-        public SectionAttributes Flag;
-        public uint Addr;
-        public uint Offset;
-        public uint Size;
-        public uint Link;
-        public uint Info;
-        public uint Addralign;
-        public uint Entsize;
-
-        public Elf32Shdr(Elf32_Shdr* st)
-        {
-            NameOffset = st->sh_name;
-            Type = (SectionType)st->sh_type;
-            Flag = (SectionAttributes)st->sh_flags;
-            Addr = st->sh_addr;
-            Offset = st->sh_offset;
-            Size = st->sh_size;
-            Link = st->sh_link;
-            Info = st->sh_info;
-            Addralign = st->sh_addralign;
-            Entsize = st->sh_entsize;
-        }
-
-    }
-
 }
--- VoPo/ELFInfo.cs
+++ VoPo/ELFInfo.cs
@@ -17,14 +17,14 @@ namespace Venera.VoPo
         public override ExitCode Execute(string[] args)
         {
 
-            if(args.Length == 0)
+            if (args.Length == 0)
             {
                 Console.WriteLine("Usage: elfinfo <path>");
                 return ExitCode.Error;
             }
 
             string path;
-            ELF elffile = null;
+            ELFFile elffile = null;
 
             if (args[0].StartsWith(@"\"))
             {
@@ -40,32 +40,86 @@ namespace Venera.VoPo
 
             try
             {
-
                 string file_content = File.ReadAllText(path);
                 byte[] binfile = Encoding.ASCII.GetBytes(file_content);
 
-                elffile = new ELF(binfile);
-                Elf32_Ehdr ehdr = elffile.getHeader();
+                elffile = new ELFFile(binfile);/*
+                Elf32_Ehdr ehdr = elffile.header;
 
-                unsafe { 
+                unsafe
+                {
                     Console.WriteLine($"ELF Header:");
-                    Console.WriteLine($"  Magic:   {ehdr.e_ident[0]:X2} {ehdr.e_ident[1]:X2} {ehdr.e_ident[2]:X2} {ehdr.e_ident[3]:X2}");
+                    Kernel.PrintDebug($"ELF Header:");
+                    Console.WriteLine($"  Magic:                             {ehdr.e_ident[0]:X2} {ehdr.e_ident[1]:X2} {ehdr.e_ident[2]:X2} {ehdr.e_ident[3]:X2}");
+                    Kernel.PrintDebug($"  Magic:                             {ehdr.e_ident[0]:X2} {ehdr.e_ident[1]:X2} {ehdr.e_ident[2]:X2} {ehdr.e_ident[3]:X2}");
                     Console.WriteLine($"  Type:                              {ehdr.e_type}");
+                    Kernel.PrintDebug($"  Type:                              {ehdr.e_type}");
                     Console.WriteLine($"  Machine:                           {ehdr.e_machine}");
+                    Kernel.PrintDebug($"  Machine:                           {ehdr.e_machine}");
                     Console.WriteLine($"  Version:                           {ehdr.e_version}");
+                    Kernel.PrintDebug($"  Version:                           {ehdr.e_version}");
                     Console.WriteLine($"  Entry point address:               {ehdr.e_entry}");
+                    Kernel.PrintDebug($"  Entry point address:               {ehdr.e_entry}");
                     Console.WriteLine($"  Start of program headers:          {ehdr.e_phoff}");
+                    Kernel.PrintDebug($"  Start of program headers:          {ehdr.e_phoff}");
                     Console.WriteLine($"  Start of section headers:          {ehdr.e_shoff}");
+                    Kernel.PrintDebug($"  Start of section headers:          {ehdr.e_shoff}");
                     Console.WriteLine($"  Flags:                             {ehdr.e_flags}");
+                    Kernel.PrintDebug($"  Flags:                             {ehdr.e_flags}");
                     Console.WriteLine($"  Size of this header:               {ehdr.e_ehsize}");
+                    Kernel.PrintDebug($"  Size of this header:               {ehdr.e_ehsize}");
                     Console.WriteLine($"  Size of program header entry:      {ehdr.e_phentsize}");
+                    Kernel.PrintDebug($"  Size of program header entry:      {ehdr.e_phentsize}");
                     Console.WriteLine($"  Number of program headers:         {ehdr.e_phnum}");
+                    Kernel.PrintDebug($"  Number of program headers:         {ehdr.e_phnum}");
                     Console.WriteLine($"  Size of section header entry:      {ehdr.e_shentsize}");
+                    Kernel.PrintDebug($"  Size of section header entry:      {ehdr.e_shentsize}");
                     Console.WriteLine($"  Number of section headers:         {ehdr.e_shnum}");
+                    Kernel.PrintDebug($"  Number of section headers:         {ehdr.e_shnum}");
                     Console.WriteLine($"  Section header string table index: {ehdr.e_shstrndx}");
+                    Kernel.PrintDebug($"  Section header string table index: {ehdr.e_shstrndx}");
                 }
 
+                List<Elf32Shdr> shdr = elffile.SectionHeaders;
+
+                Console.WriteLine("\nSection Headers:");
+                Kernel.PrintDebug("\nSection Headers:");
+                foreach (Elf32Shdr i in shdr)
+                {
+                    Console.WriteLine($"Name:       {i.Name}");
+                    Kernel.PrintDebug($"Name:       {i.Name}");
+                    Console.WriteLine($"NameOffset: {i.NameOffset}");
+                    Kernel.PrintDebug($"NameOffset: {i.NameOffset}");
+                    Console.WriteLine($"Addr:       {i.Addr}");
+                    Kernel.PrintDebug($"Addr:       {i.Addr}");
+                    Console.WriteLine($"Offset:     {i.Offset}");
+                    Kernel.PrintDebug($"Offset:     {i.Offset}");
+                    Console.WriteLine($"Size:       {i.Size}");
+                    Kernel.PrintDebug($"Size:       {i.Size}");
+                    Console.WriteLine($"Link:       {i.Link}");
+                    Kernel.PrintDebug($"Link:       {i.Link}");
+                    Console.WriteLine($"Info:       {i.Info}");
+                    Kernel.PrintDebug($"Info:       {i.Info}");
+                    Console.WriteLine($"Addalign:   {i.Addralign}");
+                    Kernel.PrintDebug($"Addalign:   {i.Addralign}");
+                    Console.WriteLine($"Entsize:    {i.Entsize}");
+                    Kernel.PrintDebug($"Entsize:    {i.Entsize}");
+                    Console.WriteLine($"");
+                    Kernel.PrintDebug($"");
+                }
+
+                List<uint> strtbl = elffile._stringTables;
+
+                Console.WriteLine("\nString Table:");
+
+                foreach (uint i in strtbl)
+                {
+                    Console.WriteLine($"String Table: {i}");
+                    Kernel.PrintDebug($"String Table: {i}");
+                }*/
+
             }
+                
             catch (Exception)
             {
                 Console.WriteLine($"Sokolsh: elfino: File {path} does not exist or is broken");
